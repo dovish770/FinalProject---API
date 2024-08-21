0fd4171fdf9b27f3e3f44e53f49da883b23245e1
dovi stein <dovish@gmail.com>
2024-08-21 13:56:17 +0300

    לחישוב זמנים ומרחקים service עידכון מודלים ויצירת

--- PojectFinal - API/Modles/Mission.cs
+++ PojectFinal - API/Modles/Mission.cs
@@ -16,40 +16,6 @@ namespace PojectFinal___API.Modles
         
         public string? TimLeft { get; set; }
 
-        public StatusMission? Status { get; set; }
-
-        //חישוב מרחק בין סוכן למטרה
-        private static double Distance(Dictionary<string, int> dic1, Dictionary<string, int> dic2)
-        {
-            int x1 = dic1["x"];
-            int y1 = dic1["y"];
-
-            int x2 = dic2["x"];
-            int y2 = dic2["y"];
-
-            var distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-
-            return distance;
-        }
-
-        //חישוב ועידכון זמן שנותר 
-        public void UpdateTimeLeft()
-        {   
-            if (this.Status.ToString() != "Completed")
-            {
-                return;
-            }
-
-            var distance = Distance(Agent.Pin, Target.Pin);            
-            this.TimLeft = (distance/5).ToString();                     
-        }
-        
-        //חישוב ועידכון משך המשימה
-        public void UpdateDuration()
-        {
-            var distance = Distance(Agent.Pin, Target.Pin);
-            var time = (distance/5);
-            this.Duration += time;
-        }
+        public StatusMission? Status { get; set; }        
     }
 }
---
+++ PojectFinal - API/Services/TimeDistanceService.cs
@@ -0,0 +1,48 @@
+﻿using PojectFinal___API.Modles;
+
+namespace PojectFinal___API.Services
+{
+    public class TimeDistanceService
+    {
+        //חישוב מרחק בין סוכן למטרה
+        public static double Distance(Dictionary<string, int> dic1, Dictionary<string, int> dic2)
+        {
+            int x1 = dic1["x"];
+            int y1 = dic1["y"];
+
+            int x2 = dic2["x"];
+            int y2 = dic2["y"];
+
+            var distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            return distance;
+        }
+
+        //חישוב ועידכון זמן שנותר 
+        public Mission UpdateTimeLeft(Mission mission)
+        {
+            if (mission.Status.ToString() != "Actice")
+            {
+                return mission;
+            }
+
+            var distance = Distance(mission.Agent.Pin, mission.Target.Pin);
+            mission.TimLeft = (distance/5).ToString();
+
+            return mission;
+        }
+
+        //חישוב ועידכון משך המשימה
+        public Mission UpdateDuration(Mission mission)
+        {
+            if (mission.Status.ToString() != "Actice")
+            {
+                return mission;
+            }
+            var distance = Distance(mission.Agent.Pin, mission.Target.Pin);
+            var time = (distance/5);
+            mission.Duration += time;
+            return mission;
+        }
+    }
+}
